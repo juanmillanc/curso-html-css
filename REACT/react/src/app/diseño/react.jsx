66c56d1b7a5dfff9ba8c008356fe322b7b5bf14b
juanmillanc <juan_millan8222@elpoli.edu.co>
2026-05-21 01:24:22 -0500

    ajuste entorno react

---
+++ REACT/react/src/app/diseño/react.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function React() {
+    return (
+        <div className="h-screen w-full bg-white flex justify-center items-center">
+            {/* contenedor medio*/}
+            <div className="w-8/10 h-8/10 bg-blue-400 border rounded-2xl text-red-600 text-4xl px-20 py-20"> hola mundo
+            <div className="2-8"></div>
+            </div>
+        </div>
+        
+    );
+}
---
+++ REACT/react/src/app/page.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import React from "./diseño/react";
+import { REACT_LOADABLE_MANIFEST } from "next/dist/shared/lib/constants";
+export default function Home() {
+    return (<React/>)
+}
