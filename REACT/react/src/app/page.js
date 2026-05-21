00ffbdd5989c92bea25a7238de7900809d5260d7
juanmillanc <juan_millan8222@elpoli.edu.co>
2026-05-21 17:28:58 -0500

    ajuste al reto-estructura

--- REACT/react/src/app/page.js
+++ REACT/react/src/app/page.js
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import React from "./diseño/react";
 import { REACT_LOADABLE_MANIFEST } from "next/dist/shared/lib/constants";
+import Estructura from "./reto1/estructura";
 export default function Home() {
-    return (<React/>)
+    return (
+    // <React/>
+        <Estructura/>
+    )
 }
