8e21d8f1878db58d3091824bb7e84dca16c99eeb
juanmillanc <juan_millan8222@elpoli.edu.co>
2026-05-21 23:58:12 -0500

    ajuste al reto estructura responsive

--- REACT/react/src/app/reto1/estructura.jsx
+++ REACT/react/src/app/reto1/estructura.jsx
@@ -2,10 +2,43 @@ import Image from "next/image";
 
 export default function Estructura() {
     return (
-        <div className="h-screen w-full bg-white flex justify-center items-center">
-            {/* contenedor medio*/}
-            <div className="w-8/10 h-8/10 bg-blue-400 border rounded-2xl text-red-600 text-4xl px-20 py-20"> hola Estructura
-            <div className="2-8"></div>
+        // body
+        <div className="h-screen flex flex-col">
+            {/* header */}
+            <div className=" flex flex-col gap-[10] p-[10] xl:flex-row">
+                {/* header-cabeza */}
+                <div className="bg-[#74B8FF] w-full p-[7] text-base font-sans text-center lg:p-[12]">Header</div>
+                {/* nav */}
+                <div className="bg-[#74B8FF] w-full p-[7] text-base font-sans text-center lg:p-[12]">Nav</div>
+            </div>
+            {/* main */}
+            <div className="p-[11]">
+                {/* content */}
+                <div className="h-full flex gap-[11] flex-col">
+                    {/* section */}
+                    <div className="bg-[#FDCA6E] h-full flex justify-center items-center p-[38] text-center flex-col lg:p-[50] xl:p-[100]">
+                        Section
+                    </div>
+                    {/* article */}
+                    <div className=" flex flex-col justify-center items-center text-center gap-[11] xl:flex-row">
+                        <div className="bg-[#54EFC3] w-full text-center gap-[11] p-[7] lg:p-[12] xl:p-[100]">Article1</div>
+                        <div className="bg-[#54EFC3] w-full text-center gap-[11] p-[7] lg:p-[12] xl:p-[100]">Article2</div>
+                        <div className="bg-[#54EFC3] w-full text-center gap-[11] p-[7] lg:p-[12] xl:p-[100]">Article3</div>
+                    </div>
+                </div>
+            </div>
+            {/* footer */}
+            <div className=" justify-center text-center flex flex-col h-screen gap-[11] p-[11] xl:flex-row">
+                {/* footer-left */}
+                <div className="flex gap-[11] flex-col lg:w-full lg:flex-row xl:flex-row">
+                    <div className="w-full p-[7] bg-[#FF7978] lg:p-[12] xl:p-[50]">Footer1</div>
+                    <div className="w-full p-[7] bg-[#FF7978] lg:p-[12] xl:p-[50]">Footer2</div>
+                </div>
+                {/* footer-rigth */}
+                <div className="flex gap-[11] flex-col lg:w-full lg:flex-row xl:flex-row">
+                    <div className="w-full p-[7] bg-[#FF7978] lg:p-[12] xl:p-[50]">Footer3</div>
+                    <div className="w-full p-[7] bg-[#FF7978] lg:p-[12] xl:p-[50]">Footer4</div>
+                </div>
             </div>
         </div>
         
