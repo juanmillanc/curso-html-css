7aafaf34361b1fa5478b73ccf188af663764ee22
juanmillanc <juan_millan8222@elpoli.edu.co>
2026-05-22 00:30:59 -0500

    elimine la carpeta nivel1

--- Nivel 1 Variables y Tipos de Datos/1. var, let y const.js
+++
@@ -1,5 +0,0 @@
-let contador = 0;
-contador = 1;
-
-const PI = 3.14;
-// PI = 3.1416; // Esto generará un error porque no se puede reasignar una constante
--- Nivel 1 Variables y Tipos de Datos/4. Objects (Objetos).js
+++
@@ -1,8 +0,0 @@
-const servidor = {
-    ip: "192.168.1.1",
-    puerto: 8080,
-    activo: true,
-    servicios: ["HTTP", "SSH"]
-};
-
-console.log(servidor.ip); // Imprime "
