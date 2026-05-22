accb5d810aaebc411bc156611cb71a948673fa15
juanmillanc <juan_millan8222@elpoli.edu.co>
2026-05-22 13:46:40 -0500

    ajuste reto card

--- REACT/react/src/app/page.js
+++ REACT/react/src/app/page.js
@@ -2,9 +2,10 @@ import Image from "next/image";
 import React from "./diseño/react";
 import { REACT_LOADABLE_MANIFEST } from "next/dist/shared/lib/constants";
 import Estructura from "./reto1/estructura";
+import Card from "./reto2/card";
 export default function Home() {
     return (
     // <React/>
-        <Estructura/>
+        <Card/>
     )
 }
